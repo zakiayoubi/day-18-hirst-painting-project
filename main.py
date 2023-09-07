c2c379870f242b6854d67d7111c0f2dc5c31014f
ayoubim <ayoubim@berea.edu>
2023-09-06 21:17:55 -0400

    Started on the project

--- main.py
+++ main.py
@@ -1,16 +1,121 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
+import turtle
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+zaki = Turtle()
+zaki.pensize(1)
+zaki.speed(0)
+paths = [0, 90, 180, 270]
+turtle.colormode(255)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_tuple = (r, g, b)
+    return color_tuple
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def colorful_circles():
+    for i in range(90):
+        zaki.pencolor(random_color())
+        zaki.circle(100)
+        zaki.right(4)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+colorful_circles()
+
+screen = Screen()
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from turtle import Turtle, Screen
+# import turtle
+# import random
+#
+# zaki = Turtle()
+# zaki.pensize(10)
+# zaki.speed(0)
+# paths = [0, 90, 180, 270]
+# turtle.colormode(255)
+#
+#
+# def random_color():
+#     r = random.randint(0, 255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     color_tuple = (r, g, b)
+#     return color_tuple
+#
+#
+# def draw_random_walk():
+#     for i in range(40):
+#         zaki.pencolor(random_color())
+#         zaki.forward(30)
+#         zaki.setheading(random.choice(paths))
+#
+#
+# draw_random_walk()
+#
+# screen = Screen()
+# screen.exitonclick()
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from turtle import Turtle, Screen
+# import random
+#
+# zaki = Turtle()
+# zaki.penup()
+# zaki.goto(-50, 100)
+# zaki.pendown()
+# num_sides = 3
+# colors = ["red", "blue", "yellow", "black", "green"]
+#
+#
+# def draw_shape():
+#     global num_sides
+#     zaki.pencolor(random.choice(colors))
+#     for i in range(num_sides):
+#         zaki.forward(100)
+#         zaki.right(360 / num_sides)
+#     num_sides += 1
+#
+#
+# while num_sides <= 10:
+#     draw_shape()
+#
+# screen = Screen()
+# screen.exitonclick()
